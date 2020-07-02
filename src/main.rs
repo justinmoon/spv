8bb39eb483fce442d9d441cdf14fdb429551533b
Justin Moon <mail@justinmoon.com>
2020-07-01 22:45:16 -0500

    Can display address

--- src/main.rs
+++ src/main.rs
@@ -5,20 +5,23 @@ use std::time::{SystemTime, UNIX_EPOCH};
 use std::{env, process};
 
 use crate::error::Error;
-use bitcoin::blockdata::{block::BlockHeader, constants::genesis_block};
+use bitcoin::blockdata::{
+    block::{Block, BlockHeader},
+    constants::genesis_block,
+    transaction::{OutPoint, TxOut},
+};
 use bitcoin::consensus::encode;
 use bitcoin::hashes::Hash;
 use bitcoin::network::stream_reader::StreamReader;
 use bitcoin::network::{
     address, constants, message,
-    message_blockdata::GetHeadersMessage,
+    message_blockdata::{GetHeadersMessage, Inventory},
     message_filter::{GetCFCheckpt, GetCFHeaders, GetCFilters},
     message_network,
 };
 use bitcoin::util::bip158::BlockFilter;
 use bitcoin::util::hash::BitcoinHash;
-use bitcoin::{BlockHash, FilterHash, Network};
-use bitcoin_hashes::sha256d;
+use bitcoin::{Address, BlockHash, FilterHash, Network};
 use rand::Rng;
 use serde::{Deserialize, Serialize};
 use serde_json;
@@ -27,6 +30,12 @@ use std::fs::File;
 use std::io::BufReader;
 
 mod error;
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq)]
+pub struct UTXO {
+    pub outpoint: OutPoint,
+    pub txout: TxOut,
+}
 
 fn build_version_message(address: SocketAddr) -> message::NetworkMessage {
     // Building version message, see https://en.bitcoin.it/wiki/Protocol_documentation#version
@@ -73,34 +82,40 @@ fn build_version_message(address: SocketAddr) -> message::NetworkMessage {
 pub struct Spv {
     //#[serde(skip)]
     //pub stream: Option<TcpStream>,
+    address: Address,
+    network: Network,
+    peer: SocketAddr,
     pub headers: Vec<BlockHeader>,
     pub checkpoints: Vec<FilterHash>,
     pub filter_headers: Vec<FilterHash>,
     pub filter_height: usize,
+    utxos: Vec<UTXO>,
 }
 
 impl Spv {
-    pub fn new() -> Self {
-        // FIXME: don't assume mainnet
-        let genesis_header = genesis_block(Network::Bitcoin).header;
+    pub fn new(network: Network, peer: SocketAddr, address: Address) -> Self {
+        // FIXME: don'k assume mainnet
+        let genesis_header = genesis_block(network).header;
         Self {
+            network,
+            address,
+            peer,
             headers: vec![genesis_header],
             checkpoints: vec![],
             filter_headers: vec![],
             filter_height: 0,
+            utxos: vec![],
         }
     }
 
     pub fn read() -> Result<Self, Error> {
         let start = std::time::Instant::now();
-        if let Ok(file) = File::open("db.json") {
-            let reader = BufReader::new(file);
-            let db: Self = serde_json::from_reader(reader)?;
-            let end = std::time::Instant::now();
-            println!("db opened in {:?}", end - start);
-            return Ok(db);
-        }
-        Ok(Self::new())
+        let file = File::open("db.json")?;
+        let reader = BufReader::new(file);
+        let db: Self = serde_json::from_reader(reader)?;
+        let end = std::time::Instant::now();
+        println!("db opened in {:?}", end - start);
+        return Ok(db);
     }
 
     pub fn save(&self) -> Result<(), Error> {
@@ -117,23 +132,10 @@ impl Spv {
 
         // This example establishes a connection to a Bitcoin node, sends the intial
         // "version" message, waits for the reply, and finally closes the connection.
-        let args: Vec<String> = env::args().collect();
-        if args.len() < 2 {
-            eprintln!("not enough arguments");
-            process::exit(1);
-        }
-
-        let str_address = &args[1];
-
-        let address: SocketAddr = str_address.parse().unwrap_or_else(|error| {
-            eprintln!("Error parsing address: {:?}", error);
-            process::exit(1);
-        });
-
-        let mut stream = TcpStream::connect(address).expect("Couldn't establish TCP connection");
+        let mut stream = TcpStream::connect(self.peer).expect("Couldn't establish TCP connection");
 
         // Start handshake
-        let version_message = build_version_message(address);
+        let version_message = build_version_message(self.peer);
         self.send_msg(&mut stream, version_message)
             .expect("Couldn't send message");
         println!("Sent version message");
@@ -156,7 +158,7 @@ impl Spv {
         payload: message::NetworkMessage,
     ) -> Result<(), Error> {
         let msg = message::RawNetworkMessage {
-            magic: constants::Network::Bitcoin.magic(),
+            magic: self.network.magic(),
             payload,
         };
         stream.write_all(encode::serialize(&msg).as_slice())?;
@@ -168,7 +170,7 @@ impl Spv {
         stream: &mut TcpStream,
         msg: message::RawNetworkMessage,
     ) -> Result<(), Error> {
-        let res = match msg.payload {
+        match msg.payload {
             message::NetworkMessage::Version(_) => {
                 println!("Received version message: {:?}", msg.payload);
                 self.send_msg(stream, message::NetworkMessage::Verack)
@@ -193,6 +195,7 @@ impl Spv {
                 println!("Sent getheaders message");
             }
             message::NetworkMessage::Headers(new_block_headers) => {
+                println!("received headers: {}", new_block_headers.len());
                 //block_headers.append(&mut block_hashes.clone());
                 for new_header in &new_block_headers {
                     if let Some(last_header) = self.headers.last() {
@@ -200,11 +203,8 @@ impl Spv {
                             continue;
                         }
                     } else {
-                        let genesis = BlockHash::from_str(
-                            &"000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f",
-                        )
-                        .unwrap();
-                        assert_eq!(genesis, new_header.bitcoin_hash());
+                        let genesis_hash = genesis_block(self.network).bitcoin_hash();
+                        assert_eq!(genesis_hash, new_header.bitcoin_hash());
                     }
 
                     self.headers.push(*new_header);
@@ -238,9 +238,9 @@ impl Spv {
                 } else {
                     // Save
                     let start = std::time::Instant::now();
-                    println!("saving db");
-                    self.save().unwrap();
-                    println!("saved db");
+                    //println!("saving db");
+                    //self.save().unwrap();
+                    //println!("saved db");
                     let end = std::time::Instant::now();
                     println!("Elapsed time saving db: {:?}", end - start);
 
@@ -272,6 +272,9 @@ impl Spv {
                 self.send_msg(stream, payload)
                     .expect("Couldn't send message");
             }
+            message::NetworkMessage::Block(block) => {
+                self.process_block(&block);
+            }
             message::NetworkMessage::CFHeaders(cfheaders) => {
                 let mut header = cfheaders.previous_filter;
                 for filter_hash in cfheaders.filter_hashes.iter() {
@@ -291,43 +294,37 @@ impl Spv {
                 let start_height = self.filter_headers.len();
                 let stop_height = std::cmp::min(
                     self.filter_headers.len() + 999,
-                    self.headers.len() - self.filter_headers.len(),
+                    //self.headers.len() - self.filter_headers.len(),
+                    self.headers.len() - 1,
                 );
-                let stop_hash = self.headers[stop_height].bitcoin_hash();
-                let payload = message::NetworkMessage::GetCFHeaders(GetCFHeaders {
-                    filter_type: 0,
-                    start_height: start_height as u32,
-                    stop_hash,
-                });
-                self.send_msg(stream, payload)
-                    .expect("Couldn't send message");
-                println!(
-                    "Received cfheaders {:?} {:?}",
-                    cfheaders.filter_hashes.len(),
-                    self.filter_headers.len()
-                );
-
-                //if stop_height < 1000 {
-                // Get Filters
-                let start_height = self.filter_headers.len();
-                if start_height >= self.headers.len() {
-                    ()
+                if stop_height >= start_height {
+                    let stop_hash = self.headers[stop_height].bitcoin_hash();
+                    let payload = message::NetworkMessage::GetCFHeaders(GetCFHeaders {
+                        filter_type: 0,
+                        start_height: start_height as u32,
+                        stop_hash,
+                    });
+                    self.send_msg(stream, payload)
+                        .expect("Couldn't send message");
+                    println!(
+                        "Received cfheaders {:?} {:?}",
+                        cfheaders.filter_hashes.len(),
+                        self.filter_headers.len()
+                    );
+                } else {
+                    // FIXME: this is a bad heuristic. If we're missing exactly 1000 filter
+                    // headers, it will fail.
+                    //if self.filter_height >= self.headers.len() {
+                    //return Ok(());
+                    //}
+                    self.request_filter(stream)?;
+                    println!("Sent getcfilters");
                 }
-                let stop_hash = self.headers[start_height].bitcoin_hash();
-                let payload = message::NetworkMessage::GetCFilters(GetCFilters {
-                    filter_type: 0,
-                    start_height: start_height as u32,
-                    stop_hash,
-                });
-                self.send_msg(stream, payload)
-                    .expect("Couldn't send message");
-                println!("Sent getcfilters");
-                //}
 
                 // FIXME: download the actual filters and check against the headers
             }
             message::NetworkMessage::CFilter(cfilter) => {
-                println!("CFilter: {:?}\n\n\n", cfilter);
+                println!("CFilter: {:?} {}\n\n\n", cfilter, self.filter_height);
                 let next_block_hash = self.headers[self.filter_height].bitcoin_hash();
 
                 // Check that
@@ -339,11 +336,14 @@ impl Spv {
                     }
                     println!("expected: {:?}", next_block_hash);
                     println!("found: {:?}", cfilter.block_hash);
-                    ()
+                    self.request_filter(stream)?;
+                    //std::thread::sleep_ms(1000);
+                    return Ok(());
                 }
 
                 // Check against header
                 let filter = BlockFilter::new(&cfilter.filter);
+
                 //let filter_hash = FilterHash::hash(filter.content.as_slice()); // Make a method
                 let previous_header = if self.filter_height == 0 {
                     FilterHash::from_str(
@@ -360,18 +360,28 @@ impl Spv {
                     println!("expected: {:?}", self.filter_headers[self.filter_height]);
                     println!("found: {:?}", header);
                     println!("block hash: {:?}", cfilter.block_hash);
-                    ()
+                    return Ok(());
                 }
 
+                // TODO: should we check block hash? or is that implied by filter header?
+
+                // Request the full block when it matches our wallet, adjusting filter height when
+                // it arrives
+                let query = vec![self.address.script_pubkey()];
+                let matches =
+                    filter.match_any(&cfilter.block_hash, &mut query.iter().map(|s| s.as_bytes()));
+                if let Ok(true) = matches {
+                    let msg = message::NetworkMessage::GetData(vec![Inventory::Block(
+                        cfilter.block_hash.clone(),
+                    )]);
+                    return self.send_msg(stream, msg);
+                } else {
+                    // If the block isn't interesting, increment our filter height
+                    self.filter_height += 1;
+                };
+
                 // Get more filters
-                let payload = message::NetworkMessage::GetCFilters(GetCFilters {
-                    filter_type: 0,
-                    start_height: self.filter_height as u32,
-                    // FIXME: why only one?
-                    stop_hash: self.headers[self.filter_height + 1].bitcoin_hash(),
-                });
-                self.send_msg(stream, payload)
-                    .expect("Couldn't send message");
+                self.request_filter(stream)?;
             }
             _ => {
                 println!("Received unknown message: {:?}", msg.cmd());
@@ -380,13 +390,96 @@ impl Spv {
         Ok(())
     }
 
-    fn get_block(&self, block_id: &sha256d::Hash) -> Result<(), Error> {
+    fn request_filter(&mut self, stream: &mut TcpStream) -> Result<(), Error> {
+        //let stop_hash = self.headers[self.filter_height + 1].bitcoin_hash();
+        if self.headers.len() > self.filter_height {
+            let stop_hash = self.headers[self.filter_height].bitcoin_hash();
+            println!("requesting {} -> {}", self.filter_height, stop_hash);
+            let payload = message::NetworkMessage::GetCFilters(GetCFilters {
+                filter_type: 0,
+                start_height: self.filter_height as u32,
+                // FIXME: why only one?
+                stop_hash,
+            });
+            self.send_msg(stream, payload)
+                .expect("Couldn't send message");
+        }
         Ok(())
+    }
+
+    fn process_block(&mut self, block: &Block) {
+        let block_hash = block.bitcoin_hash();
+        let expected_hash = self.headers[self.filter_height].bitcoin_hash();
+        if block_hash != expected_hash {
+            println!("can't process this block yet");
+            return;
+        }
+        // Copying bitcoin-wallet
+        for tx in block.txdata.iter() {
+            // Just look for outputs for now
+            for (vout, output) in tx.output.iter().enumerate() {
+                if output.script_pubkey == self.address.script_pubkey() {
+                    let outpoint = OutPoint {
+                        txid: tx.txid(),
+                        vout: vout as u32,
+                    };
+                    let utxo = UTXO {
+                        txout: output.clone(),
+                        outpoint,
+                    };
+                    self.utxos.push(utxo);
+                    println!("balance {:?}", self.balance());
+                }
+            }
+        }
+
+        // This filter has been fully processed ...
+        self.filter_height += 1;
+    }
+
+    fn balance(&self) -> u64 {
+        self.utxos.iter().map(|u| u.txout.value).sum()
     }
 }
 
+fn exit(msg: &str) {
+    eprintln!("{}", msg);
+    process::exit(1);
+}
+
 fn main() {
-    let mut spv = Spv::new();
+    // TODO: parse args here
+    let address = Address::from_str("bcrt1qs0wzcfx0ltany6gq43ds3w88kcuxu4umxdrx5u").unwrap();
+    let network = Network::Bitcoin;
+    let network = Network::Regtest;
+
+    // Parse CLI
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 4 {
+        exit("usage: spv <mainnet|regtest|testnet> <peer-ip> <bitcoin-address>");
+    }
+
+    let network = match &args[1][..] {
+        "mainnet" => Network::Bitcoin,
+        "testnet" => Network::Testnet,
+        "regtest" => Network::Regtest,
+        _ => {
+            eprintln!("usage: spv <mainnet|regtest|testnet> <peer-ip> <bitcoin-address>");
+            process::exit(1);
+        }
+    };
+
+    let peer: SocketAddr = args[2].parse().unwrap_or_else(|error| {
+        eprintln!("Error parsing peer address: {:?}", error);
+        process::exit(1);
+    });
+
+    let address: Address = args[3].parse().unwrap_or_else(|error| {
+        eprintln!("Error parsing bitcoin address: {:?}", error);
+        process::exit(1);
+    });
+
+    let mut spv = Spv::new(network, peer, address);
     spv.run();
 }
 
@@ -404,13 +497,11 @@ mod tests {
         let header: BlockHeader =
             deserialize(&raw).expect("Can't deserialize correct block header");
 
-        let headers = vec![header, header, header];
-        let spv = Spv {
-            headers,
-            checkpoints: vec![],
-            filter_headers: vec![],
-            filter_height: 0,
-        };
+        // FIXME: this is weird
+        let address = Address::from_str("bcrt1qs0wzcfx0ltany6gq43ds3w88kcuxu4umxdrx5u").unwrap();
+        let spv = Spv::new(Network::Regtest, address);
+        spv.headers = vec![header, header, header];
+
         spv.save().unwrap();
         let from_disk = Spv::read().unwrap();
         assert_eq!(spv, from_disk);
